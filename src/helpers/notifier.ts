836fa9d19e9c88ee0c0730bd66b9f4afd9f43b53
Erez Alster <erez@alster.co.il>
2020-01-21 10:02:22 +0200

    Don't send emails if you are the only watcher

--- src/helpers/notifier.ts
+++ src/helpers/notifier.ts
@@ -31,17 +31,19 @@ export const notifyComment = async (user: IUserInstance, doc: IDocInstance, comm
         }) as any;
 
         // Remove current user email address
-        watchers = watchers.filter((watcher) => watcher.userId !== user.id && user.email);
+        // watchers = watchers.filter((watcher) => watcher.userId !== user.id && user.email);
 
-        const emails = watchers.map((watcher) => watcher.user.email);
-        const msg = {
-            from: `${user.displayName || user.username} (Ahora) <${doc.id}-${comment.id}-comment@${EMAIL_DOMAIN}>`,
-            to: emails,
-            templateId: 'd-8b18787f0f5c47339cd670bfb1c6a6b5',
-            dynamic_template_data: { user, doc, comment, organization, url: URL },
-        };
-        await sgMail.send(msg).catch((error: any) => {
-            console.error(error.response.body);
-        });
+        if (watchers.length > 0) {
+            const emails = watchers.map((watcher) => watcher.user.email);
+            const msg = {
+                from: `${user.displayName || user.username} (Ahora) <${doc.id}-${comment.id}-comment@${EMAIL_DOMAIN}>`,
+                to: emails,
+                templateId: 'd-8b18787f0f5c47339cd670bfb1c6a6b5',
+                dynamic_template_data: { user, doc, comment, organization, url: URL },
+            };
+            await sgMail.send(msg).catch((error: any) => {
+                console.error(error.response.body);
+            });
+        }
     }
 }
