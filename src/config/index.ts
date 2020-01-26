5c3494541746666731665cdf2e15dd21e6f4e27e
Erez Alster <erez@alster.co.il>
2020-01-26 11:33:38 +0200

    remove config DB connection string

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const DB_CONNECTION_STRING: string = process.env.DB_CONNECTION_STRING as string || "postgres://postgresadmin:admin123@ahora.usersys.redhat.com/postgresdb";
+export const DB_CONNECTION_STRING: string = process.env.DB_CONNECTION_STRING as string;
 export const GIT_HUB_CALLBACK_URL: string = process.env.GIT_HUB_CALLBACK_URL as string || "http://127.0.0.1:3000/auth/github/callback";
 export const MODE: string = process.env.MODE || "development";
 export const GIT_HUB_CLIENT_ID: string = process.env.GIT_HUB_CLIENT_ID as string;
