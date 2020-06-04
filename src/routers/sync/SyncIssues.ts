961e63d6259ba68e00d1c38a9fde1fee1eb98f8d
Erez Alster <erez@alster.co.il>
2020-06-04 13:25:34 +0300

    revert last commit

--- src/routers/sync/SyncIssues.ts
+++ src/routers/sync/SyncIssues.ts
@@ -23,7 +23,6 @@ const generateDocHTML = async (doc: Doc): Promise<Doc> => {
                 }).
                 use(html).process(doc.description as any, (error, file) => {
                     doc.htmlDescription = String(file);
-                    doc.htmlDescription = doc.htmlDescription.replace(/\n/g, "<br />");
                     resolve(doc);
                 });
         }
