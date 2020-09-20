1afd57d4f17e10ca8a6606f839c30440a59df29b
Erez Alster <erez@alster.co.il>
2020-09-20 23:31:46 +0300

    Add integrnal api to get access tokens

--- src/routers/internal/docSources.ts
+++ src/routers/internal/docSources.ts
@@ -1,10 +1,11 @@
 import express, { Router, Request, Response, NextFunction, request } from "express";
 import DocSource from "../../models/docSource";
-import { Op } from "sequelize";
+import { Op, where } from "sequelize";
 import Organization from "../../models/organization";
 import db from "../../models";
-import OrganizationTeamUser from "../../models/organizationTeamsUsers";
+import users from "../users";
 import User from "../../models/users";
+import OrganizationTeamUser from "../../models/organizationTeamsUsers";
 
 const router: Router = express.Router();
 
@@ -13,12 +14,6 @@ router.get("/internal/docsources", async (req: Request, res: Response, next: Nex
         var since = new Date();
         since.setMinutes(since.getMinutes() - 3);
         const docSources = await DocSource.findAll({
-            where: {
-                [Op.or]: [
-                    { lastUpdated: { [Op.lte]: since } },
-                    { lastUpdated: null }
-                ]
-            },
             include: [
                 { model: Organization, as: "organizationFK", attributes: ["login"] }
             ],
@@ -31,12 +26,32 @@ router.get("/internal/docsources", async (req: Request, res: Response, next: Nex
     }
 });
 
+
+
 router.put("/internal/docsources/:docSourceId", async (req: Request, res: Response, next: NextFunction) => {
     try {
         await DocSource.update(req.body, {
             where: { id: req.params.docSourceId }
         });
         res.send();
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/internal/organizations/:organizationId/accesstokens", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const organizationId = parseInt(req.params.organizationId);
+        const users = await User.findAll({
+            attributes: ["accessToken"],
+            where: {
+                accessToken: { [Op.not]: null }
+            },
+            include: [
+                { model: OrganizationTeamUser, where: { organizationId }, attributes: [] }
+            ]
+        });
+        res.send(users.map((user) => user.accessToken));
     } catch (error) {
         next(error);
     }
